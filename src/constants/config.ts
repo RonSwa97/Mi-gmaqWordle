6344fe23f43f6f87c079796bb4687a7e1c88c402
RonSwa97 <ron.swa@hotmail.com>
2025-04-15 18:31:38 -0300

    commit 3

--- src/constants/config.ts
+++ src/constants/config.ts
@@ -2,15 +2,15 @@ export const CONFIG = {
   tries: 7, // This changes how many tries you get to finish the game
   language: "Mi'gmaq", // This changes the display name for your language
   wordLength: 5, // This sets how long each word is based on how many characters (as defined in orthography.ts) are in each word
-  author: "test", // Put your name here so people know who made this game!
-  authorWebsite: "test", // Put a link to your website or social media here
-  wordListSource: "test", // Describe the source material for your words here
+  author: "Ron", // Put your name here so people know who made this game!
+  authorWebsite: "none", // Put a link to your website or social media here
+  wordListSource: "temp", // Describe the source material for your words here
   wordListSourceLink: "test", // Put a link to the source material for your words here
   //
   // THESE NEXT SETTINGS ARE FOR ADVANCED USERS
   //
   googleAnalytics: "", // You can use this if you use Google Analytics
-  shuffle: true, // whether to shuffle the words in the wordlist each time you load the app (note: you will lose the 'word of the day' functionality if this is true)
+  shuffle: false, // whether to shuffle the words in the wordlist each time you load the app (note: you will lose the 'word of the day' functionality if this is true)
   normalization: "NFC", // whether to apply Unicode normalization to words and orthography - options: 'NFC', 'NFD', 'NKFC', 'NKFD', false
   startDate: "April 14, 2025 00:00:00", // what date and time to start your game from
   defaultLang: "en", // the default interface language
