83d689fd0f0266d8bb133b69d405a2f56e66e1a5
RonSwa97 <ron.swa@hotmail.com>
2025-04-16 13:14:47 -0300

    commit 9

--- src/constants/config.ts
+++ src/constants/config.ts
@@ -1,7 +1,7 @@
 export const CONFIG = {
   tries: 7, // This changes how many tries you get to finish the game
   language: "Mi'gmaq", // This changes the display name for your language
-  wordLength: 5, // This sets how long each word is based on how many characters (as defined in orthography.ts) are in each word
+  wordLength: 6, // This sets how long each word is based on how many characters (as defined in orthography.ts) are in each word
   author: "Ron", // Put your name here so people know who made this game!
   authorWebsite: "none", // Put a link to your website or social media here
   wordListSource: "Mi'gmaq Online Talking Dictionary", // Describe the source material for your words here
