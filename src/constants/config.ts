411837346f414e0aa47e7616a2e95a4e59f93089
RonSwa97 <ron.swa@hotmail.com>
2025-04-15 21:24:10 -0300

    commit 7

--- src/constants/config.ts
+++ src/constants/config.ts
@@ -4,13 +4,13 @@ export const CONFIG = {
   wordLength: 5, // This sets how long each word is based on how many characters (as defined in orthography.ts) are in each word
   author: "Ron", // Put your name here so people know who made this game!
   authorWebsite: "none", // Put a link to your website or social media here
-  wordListSource: "temp", // Describe the source material for your words here
-  wordListSourceLink: "test", // Put a link to the source material for your words here
+  wordListSource: "Mi'gmaq Online Talking Dictionary", // Describe the source material for your words here
+  wordListSourceLink: "https://mikmaqonline.org/", // Put a link to the source material for your words here
   //
   // THESE NEXT SETTINGS ARE FOR ADVANCED USERS
   //
   googleAnalytics: "", // You can use this if you use Google Analytics
-  shuffle: false, // whether to shuffle the words in the wordlist each time you load the app (note: you will lose the 'word of the day' functionality if this is true)
+  shuffle: true, // whether to shuffle the words in the wordlist each time you load the app (note: you will lose the 'word of the day' functionality if this is true)
   normalization: "NFC", // whether to apply Unicode normalization to words and orthography - options: 'NFC', 'NFD', 'NKFC', 'NKFD', false
   startDate: "April 14, 2025 00:00:00", // what date and time to start your game from
   defaultLang: "en", // the default interface language
